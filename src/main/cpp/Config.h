b897827728784de89cf2b1305ee8fbf1b893c299
Janez Radescek <janez.radescek@dhimahi.com>
2024-11-11 10:03:22 +0100

    c++ echo

---
+++ src/main/cpp/Config.h
@@ -0,0 +1,17 @@
+//
+// Created by janez on 11.11.2024.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+
+class Config {
+public:
+    const static int PORT = 8080;
+};
+
+
+
+#endif //CONFIG_H
---
+++ src/main/cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void run_server();
+
+int main()
+{
+    std::cout << "Starting WebSocket server..." << std::endl;
+    run_server();
+    return 0;
+}
