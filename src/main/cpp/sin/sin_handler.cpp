0224e3d5bce180be7fdcd7706982dac54e26000e
Janez Radescek <janez.radescek@dhimahi.com>
2024-11-14 23:44:43 +0100

    c++ second interval broadcast

--- src/main/cpp/sin/sin_handler.cpp
+++ src/main/cpp/sin/sin_handler.cpp
@@ -17,19 +17,38 @@ void on_msg(server *s, websocketpp::connection_hdl hdl, server::message_ptr msg)
     s->send(hdl, message, msg->get_opcode());
 }
 
-Task process_one_step(const Task &task) {
-    double number = task.input;
-    double result = std::sin(number);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1)); // Simulate long processing time
-    return Task(task.input, result, 0, task.step + 1, task.required_steps);
+double sinOneTerm(double term, double x, int n) {
+    return term * -x * x / ((2 * n) * (2 * n + 1));
 }
+
+Task sinOneStep(const Task &task) {
+    double x = task.input;
+    // x = fmod(x, 2 * M_PI); // Technically, it converges on the whole ℝ, so ¯\_(ツ)_/¯. Since we are technical, it's not even in line with the task.
+    double term = task.term;
+    int step = task.step;
+    double newTerm = step == 0 ? x : sinOneTerm(term, x, step);
+    double newOutput = task.output + newTerm;
+    std::this_thread::sleep_for(std::chrono::milliseconds(300)); // Simulate long processing time
+    std::cout << "Input: " << x << ", Step: " << step << ", New value: " << newOutput << std::endl;
+    return Task(task.input, newOutput, newTerm, task.step + 1, task.required_steps);
+}
+
+
 
 void process_all_steps(const Msg &msg, const std::function<void(const std::string &)> &broadcast_message) {
     auto current_task = msg.task;
+    auto start = std::chrono::steady_clock::now();
+
     while (current_task.step < current_task.required_steps) {
-        current_task = process_one_step(current_task);
-        broadcast_message(Msg(msg.id, MessageType::PARTIAL, current_task, "").toString());
+        current_task = sinOneStep(current_task);
+
+        auto now = std::chrono::steady_clock::now();
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count() >= 1000) {
+            broadcast_message(Msg(msg.id, MessageType::PARTIAL, current_task, "").toString());
+            start = now;
+        }
     }
+    broadcast_message(Msg(msg.id, MessageType::RESULT, current_task, "").toString());
 }
 
 void process_and_broadcast(const std::string &message,
